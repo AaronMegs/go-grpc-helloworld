ed2119a4b7c677cabe39431b57584a54d93e8761
AaronMegs <aaronmegs@163.com>
2022-01-03 17:48:27 +0800

    add: Server and Client code

---
+++ helloworld/server/main.go
@@ -0,0 +1,38 @@
+// Package main implements a client for Greeter service.
+package main
+
+import (
+	"context"
+	pb "github.com/aaronmegs/go-grpc-helloworld/helloword/protos"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement protos.GreeterServer
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements portos.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName)
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
+}
